4d9ac6a93c53e1cf5b59bbe1e0402e850c3ca2a4
WebMasterIT! <wspc777@yandex.ru>
2020-07-11 22:23:57 +0300

    Add all elements

--- script/script.js
+++ script/script.js
@@ -1,147 +1,168 @@
-<<<<<<< HEAD
-const modalAdd = document.querySelector('.modal__add'),
-      addAd = document.querySelector('.add__ad'),
-      modalBtnSubmit = document.querySelector('.modal__btn-submit'),
-      modalSubmit = document.querySelector('.modal__submit'),
-      catalog = document.querySelector('.catalog'),
-      modalItem = document.querySelector('.modal__item'),
-      modalBtnWarning = document.querySelector('.modal__btn-warning');
+const dataBase = JSON.parse(localStorage.getItem("awito")) || [];
+
+let counter = dataBase.length;
+
+const modalAdd = document.querySelector(".modal__add"),
+  addAd = document.querySelector(".add__ad"),
+  modalBtnSubmit = document.querySelector(".modal__btn-submit"),
+  modalSubmit = document.querySelector(".modal__submit"),
+  catalog = document.querySelector(".catalog"),
+  modalItem = document.querySelector(".modal__item"),
+  modalBtnWarning = document.querySelector(".modal__btn-warning"),
+  modalFileInput = document.querySelector(".modal__file-input"),
+  modalFileBtn = document.querySelector(".modal__file-btn"),
+  modalImageAdd = document.querySelector(".modal__image-add");
 
 
-const dataBase = [];
+const modalImageItem = document.querySelector('.modal__image-item'),
+modalHeaderItem  = document.querySelector('.modal__header-item'),
+modalStatusItem = document.querySelector('.modal__status-item'),
+modalDescriptionItem = document.querySelector('.modal__description-item'),
+modalCostItem = document.querySelector('.modal__cost-item');
 
-const elementsModalSubmit = [...modalSubmit.elements]
-.filter(elem => elem.tagName !== 'BUTTON' && elem.type !== 'submit');
+const searchInput = document.querySelector('.search__input'),
+    menuContainer = document.querySelector('.menu__container');
 
-     
-   
-const closeModal = event => {
-    const target = event.target;
+const textFileBtn = modalFileBtn.textContent;
+const srcModalImage = modalImageAdd.src;
 
-    if (target.closest('.modal__close') || target.classList.contains('modal') 
-        || event.code = 'Escape' ) {
-        
-        modalSubmit.reset();
-        modalItem.classList.add('hide'); 
-        modalAdd.classList.add('hide');
-        document.removeEventListener('keydown', closeModal);
-    }
-})
+const elementsModalSubmit = [...modalSubmit.elements].filter(
+  (elem) => elem.tagName !== "BUTTON" && elem.type !== "submit"
+);
 
+const saveDB = () => localStorage.setItem("awito", JSON.stringify(dataBase));
 
-modalSubmit.addEventListener('input', () => {
-    const validForm = elementsModalSubmit.every( elem => elem.value);
-    modalBtnSubmit.disabled =  !validForm;
-    modalBtnWarning.style.display = validForm ? 'none' : '';
-})
+const infoPhoto = {};
 
-modalSubmit.addEventListener('submit', event => {
-    event.preventDefault();
-    const itemObj = {};
-    for (const elem of elementsModalSubmit) {
-        itemObj[elem.name] = elem.value;
-    }
+const checkForm = () => {
+  const validForm = elementsModalSubmit.every((elem) => elem.value);
+  modalBtnSubmit.disabled = !validForm;
+  modalBtnWarning.style.display = validForm ? "none" : "";
+};
 
-    dataBase.push(itemObj);
+const closeModal = (event) => {
+  const target = event.target;
+
+  if (
+    target.closest(".modal__close") ||
+    target.classList.contains("modal") ||
+    event.code === "Escape") 
+    {
     modalSubmit.reset();
+    modalItem.classList.add("hide");
+    modalAdd.classList.add("hide");
+    modalImageAdd.src = srcModalImage;
+    modalFileBtn.textContent = textFileBtn;
+    document.removeEventListener("keydown", closeModal);
+    checkForm();
+  }
+};
 
-})
+const renderCard = (DB = dataBase) => {
+  catalog.textContent = "";
 
-addAd.addEventListener('click', () => {
-    modalAdd.classList.remove('hide');
-    modalBtnSubmit.disabled = true;
-    document.addEventListener('keydown', closeModal);
-});
+  DB.forEach(item => {
+    catalog.insertAdjacentHTML(
+      "beforeend",
+      `<li class="card" data-id="${item.id}">
+        <img class="card__image" src="data:image/jpeg;base64,${item.image}" alt="test">
+        <div class="card__description">
+            <h3 class="card__header">${item.nameItem}</h3>
+            <div class="card__price">${item.costItem} ₽</div>
+        </div>
+      </li>
+      `
+    );
+  });
+};
 
-catalog.addEventListener('click', event => {
-    const target = event.target;
-
-    if (target.closest('.card')) {
-        modalItem.classList.remove('hide');
-        document.addEventListener('keydown', closeModal);
-    };
-
-});
-
-modalAdd.addEventListener('click', closeModal);
-modalItem.addEventListener('click', closeModal);
-
-
-
+searchInput.addEventListener('input', () => {
+    const valueSearch =  searchInput.value.trim().toLowerCase();
     
-
-
-=======
-const modalAdd = document.querySelector('.modal__add'),
-      addAd = document.querySelector('.add__ad'),
-      modalBtnSubmit = document.querySelector('.modal__btn-submit'),
-      modalSubmit = document.querySelector('.modal__submit'),
-      catalog = document.querySelector('.catalog'),
-      modalItem = document.querySelector('.modal__item'),
-      modalBtnWarning = document.querySelector('.modal__btn-warning');
-
-
-const dataBase = [];
-
-const elementsModalSubmit = [...modalSubmit.elements]
-.filter(elem => elem.tagName !== 'BUTTON' && elem.type !== 'submit');
-
-     
-   
-const closeModal = ("keydown",function(event) {
-    const target = event.target;
-
-    if (target.closest('.modal__close') || target === modalItem || 
-        event.key === 'Escape' || target === modalAdd ) {
-        
-        modalSubmit.reset();
-        modalItem.classList.add('hide'); 
-        modalAdd.classList.add('hide');
-        document.removeEventListener('keydown', closeModal);
+    if(valueSearch.length < 2) {
+        const result = dataBase.filter(item => item.nameItem.toLowerCase().includes(valueSearch) ||
+                                       item.descriptionItem.toLowerCase().includes(valueSearch));
+        renderCard(result);
     }
 })
 
+modalFileInput.addEventListener("change", (event) => {
+  const target = event.target;
 
-modalSubmit.addEventListener('input', () => {
-    const validForm = elementsModalSubmit.every( elem => elem.value);
-    modalBtnSubmit.disabled =  !validForm;
-    modalBtnWarning.style.display = validForm ? 'none' : '';
-})
+  const reader = new FileReader();
 
-modalSubmit.addEventListener('submit', event => {
-    event.preventDefault();
-    const itemObj = {};
-    for (const elem of elementsModalSubmit) {
-        itemObj[elem.name] = elem.value;
+  const file = target.files[0];
+
+  infoPhoto.filename = file.name;
+  infoPhoto.size = file.size;
+
+  reader.readAsBinaryString(file);
+
+  reader.addEventListener("load", (event) => {
+    if (infoPhoto.size < 300000) {
+      modalFileBtn.textContent = infoPhoto.filename;
+      infoPhoto.base64 = btoa(event.target.result);
+      modalImageAdd.src = `data:image/jpeg;base64,${infoPhoto.base64}`;
+    } else {
+      modalFileBtn.textContent = "Размер файла не должен превышать 300кб";
+      modalFileInput.value = '';
+      checkForm();
     }
+  });
+});
 
-    dataBase.push(itemObj);
-    modalSubmit.reset();
+modalSubmit.addEventListener("input", checkForm);
 
+modalSubmit.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const itemObj = {};
+  
+  for (const elem of elementsModalSubmit) {
+    itemObj[elem.name] = elem.value;
+  }
+  itemObj.id = counter++;
+  itemObj.image = infoPhoto.base64;
+  dataBase.push(itemObj);
+  closeModal({ target: modalAdd });
+  saveDB();
+  renderCard();
+});
+
+addAd.addEventListener("click", () => {
+  modalAdd.classList.remove("hide");
+  modalBtnSubmit.disabled = true;
+  document.addEventListener("keydown", closeModal);
+});
+
+catalog.addEventListener("click", (event) => {
+  const target = event.target;
+  const card = target.closest('.card');
+
+  if (card) {
+    const item = dataBase.find( obj => obj.id === +card.dataset.id);
+
+    modalImageItem.src = `data:image/jpeg;base64,${item.image}`;
+    modalHeaderItem.textContent = item.nameItem;
+    modalStatusItem.textContent = item.status === 'new' ? 'Новый' : 'Б/У';
+    modalDescriptionItem.textContent = item.descriptionItem;
+    modalCostItem.textContent = item.costItem;
+
+    modalItem.classList.remove("hide");
+    document.addEventListener("keydown", closeModal);
+  }
+});
+
+menuContainer.addEventListener('click', event => {
+  const target = event.target;
+
+  if (target.tagName === 'A') {
+    const result = dataBase.filter(item => item.category === target.dataset.category);
+
+    renderCard(result);
+  }
 })
 
-addAd.addEventListener('click', () => {
-    modalAdd.classList.remove('hide');
-    modalBtnSubmit.disabled = true;
-    document.addEventListener('keydown', closeModal);
-});
+modalAdd.addEventListener("click", closeModal);
+modalItem.addEventListener("click", closeModal);
 
-catalog.addEventListener('click', event => {
-    const target = event.target;
-
-    if (target.closest('.card')) {
-        modalItem.classList.remove('hide');
-        document.addEventListener('keydown', closeModal);
-    };
-
-});
-
-modalAdd.addEventListener('click', closeModal);
-modalItem.addEventListener('click', closeModal);
-
-
-
-    
-
-
->>>>>>> d95e2ead0df9b2f0ac052c7d1d373c55280371df
+renderCard();
